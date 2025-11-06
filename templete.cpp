a4121035e1204c1edf0eafcd3f3b68787ef98147
SandipanNayek <sandipannayek09@gmail.com>
2025-11-06 09:44:48 +0530

    Update templete.cpp

--- templete.cpp
+++ templete.cpp
@@ -86,6 +86,30 @@ vector<int> sieve(int n) {  /// prime numbers
     }
     return prime;
 }
+
+// 3SUM
+int threeSumClosest(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int closest = nums[0] + nums[1] + nums[2]; // initial sum
+
+    for(int i = 0; i < n - 2; i++) {
+        if(i > 0 && nums[i] == nums[i-1]) continue; // skip duplicates if needed
+
+        int left = i + 1, right = n - 1;
+        while(left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(abs(sum - target) < abs(closest - target))
+                closest = sum; // update closest
+
+            if(sum < target) left++;
+            else if(sum > target) right--;
+            else return target; // exact match
+        }
+    }
+    return closest;
+}
 void solve(){
     // write your code
 }
