36bfd6c1e5120a22d51d11aa90283e55d2b565ce
SandipanNayek <sandipannayek09@gmail.com>
2026-03-19 23:54:14 +0530

    topological sort

--- toposort.cpp
+++ toposort.cpp
@@ -1,71 +1,29 @@
-#include<stdio.h>
-
-
-struct Node
-{
-   int data;
-   struct Node *next;
-}*top = NULL;
-
-void push(int);
-void pop();
-void display();
-
-void main()
-{
-   int choice, value;
-   clrscr();
-   printf("\n:: Stack using Linked List ::\n");
-   while(1){
-      printf("\n****** MENU ******\n");
-      printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
-      printf("Enter your choice: ");
-      scanf("%d",&choice);
-      switch(choice){
-	 case 1: printf("Enter the value to be insert: ");
-		 scanf("%d", &value);
-		 push(value);
-		 break;
-	 case 2: pop(); break;
-	 case 3: display(); break;
-	 case 4: exit(0);
-	 default: printf("\nWrong selection!!! Please try again!!!\n");
-      }
-   }
-}
-void push(int value)
-{
-   struct Node *newNode;
-   newNode = (struct Node*)malloc(sizeof(struct Node));
-   newNode->data = value;
-   if(top == NULL)
-      newNode->next = NULL;
-   else
-      newNode->next = top;
-   top = newNode;
-   printf("\nInsertion is Success!!!\n");
-}
-void pop()
-{
-   if(top == NULL)
-      printf("\nStack is Empty!!!\n");
-   else{
-      struct Node *temp = top;
-      printf("\nDeleted element: %d", temp->data);
-      top = temp->next;
-      free(temp);
-   }
-}
-void display()
-{
-   if(top == NULL)
-      printf("\nStack is Empty!!!\n");
-   else{
-      struct Node *temp = top;
-      while(temp->next != NULL){
-	 printf("%d--->",temp->data);
-	 temp = temp -> next;
-      }
-      printf("%d--->NULL",temp->data);
-   }
+vector<int> topo; //Stores lexicographically smallest toposort
+vector<int> g[N];
+bool toposort() //Returns 1 if there exists a toposort, 0 if there is a cycle
+{   
+    priority_queue<int, vector<int>, greater<int> > pq;
+    for(int i=1;i<=n;i++)
+        for(auto &it:g[i])
+            indeg[it]++;
+    for(int i=1;i<=n;i++)
+    {
+        if(!indeg[i])
+            pq.push(i);
+    }
+    while(!pq.empty())
+    {
+        int u=pq.top();
+        pq.pop();
+        topo.push_back(u);
+        for(auto &v:g[u])
+        {
+            indeg[v]--;
+            if(!indeg[v])
+                pq.push(v);
+        }
+    }
+    if(topo.size()<n)
+        return 0;
+    return 1;
 }
